ee5df1c2050719dc03fdb5e44ca8072b1a60de5e
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-02-15 12:55:27 +0900

    [Improve] error check

--- flood_fill.c
+++ flood_fill.c
@@ -19,40 +19,66 @@ void	print_map(int *map, int y, int x)
 	printf("\n");
 }
 
-void	check_fill(int *map, int y, int x, int p_y, int p_x)
+void	check_fill(int *map, int y, int x, int p_y, int p_x, int *false_checker)
 {
-	if (map[p_y * x + p_x] == 3 || map[p_y * x + p_x] == 1)
+	if (map[p_y * x + p_x] == 3 || map[p_y * x + p_x] == 1 || *false_checker == 1)
 		return ;
-	print_map(map, 8, 18);
+//	print_map(map, 9, 18);
 	if (map[p_y * x + p_x] == -1)
 	{
-		printf("error");
+		*false_checker = 1;
 		return ;
 	}
 	if (map[p_y * x + p_x] == 0)
 		map[p_y * x + p_x] = 3;
-	check_fill(map, y, x, p_y - 1, p_x);
-	check_fill(map, y, x, p_y, p_x + 1);
-	check_fill(map, y, x, p_y + 1, p_x);
-	check_fill(map, y, x, p_y, p_x - 1);
-	
+	check_fill(map, y, x, p_y - 1, p_x, false_checker);
+	check_fill(map, y, x, p_y, p_x + 1, false_checker);
+	check_fill(map, y, x, p_y + 1, p_x, false_checker);
+	check_fill(map, y, x, p_y, p_x - 1, false_checker);
 }
+
+/*
+void	put_grid_to_container(int *map, int *cont_p)
+{
+	int i = 0, j = 0;
+	int flag_y = -1, flag_x = -1;
+	while (i < 100)
+	{
+		j = 0;
+		flag_x = -1
+		while (j < 100)
+		{
+			if (map[] )
+			cont_p[i * 100 + j] 
+			j++;
+		}
+		i++;
+	}
+}
+*/
 
 int main()
 {
-	int grid[8][18] = {
+	int grid[9][18] = {
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-		{1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 0, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-		{1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 			};
+//	int container[100][100];
 	int *map = (int *)grid;
-	print_map(map, 8, 18);
-	check_fill(map, 8, 18, 4, 8);
-	print_map(map, 8, 18);
+//	int *cont_p = (int *)container;
+	int false_checker = -1;
+//	put_grid_to_container(map, cont_p);
+	print_map(map, 9, 18);
+	check_fill(map, 9, 18, 3, 8, &false_checker);
+	print_map(map, 9, 18);
+	if (false_checker == 1)
+		printf("\nError: Map is not sorrounded by wall.\n");
 	return (0);
 }
